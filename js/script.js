c40b2920b7ff864ef2c976599f20c98ca1948453
dwbecker <dwbecker89@gmail.com>
2021-06-09 12:21:58 -0400

    added button and js for the button

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//find element on the page
+const actionButton = document.getElementById("action-btn")
+
+//perform action on click event
+
+actionButton.addEventListener("click", performActionOnClick)
+
+function performActionOnClick() {
+    alert("You've been rick-rolled!")
+}
