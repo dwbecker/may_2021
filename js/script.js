7e91cf8d3fd5bfd1a423c8768c8edff21200ba10
dwbecker <dwbecker89@gmail.com>
2021-07-14 12:24:56 -0400

    updated CSS

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 //find element on the page
-const menuicon = document.getElementById("menu-icon");
-const listItems = document.getElementById("list-items");
-const navMenu = document.getElementById('nav-menu');
+const menuIcon = document.getElementById("menu-icon");
+const navMenu = document.getElementById("nav-menu");
 
 //perform action on click event
 
-menu-icon.addEventListener("click", showNavMenuOnClick);
+menuIcon.addEventListener("click", showNavMenuOnClick);
 
 // This is the hamburger menu
 function showNavMenuOnClick() {
-    if(!navMenu.style.display) { // equal to (navMenu.style.display === '')
+    if(!navMenu.style.display) {
         navMenu.style.display = 'flex'
     } else {
         navMenu.style.display = '';
     }
 }
-
